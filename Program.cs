918d04136061ad8bdcf2527144d0831c9903366f
Carl Walsh <darthwalsh@gmail.com>
2017-03-16 23:05:38 -0700

    add code from dropbox

---
+++ Program.cs
@@ -0,0 +1,116 @@
+﻿// PascalsTriangle, by Carl Walsh Sept 2010
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace Pascals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ROWS = 0;
+            int MOD = 0;
+            bool MAKE_IMAGE = false;
+            Console.WriteLine("How many rows?...");
+            String reply = Console.ReadLine();
+            try
+            {
+                ROWS = Int32.Parse(reply);
+            }
+            catch { }
+            Console.WriteLine("Modded to?");
+            reply = Console.ReadLine();
+            try
+            {
+                MOD = Int32.Parse(reply);
+            }
+            catch { }
+            if (ROWS < 1)
+                ROWS = 1;
+            if (MOD < 1)
+                MOD = 1;
+            Console.WriteLine("Creating a Pascal's Triangle with {0} rows, mod {1}", ROWS, MOD);
+            Triangle tri = new Triangle(ROWS, MOD);
+            Console.WriteLine("Display as image, or as text? (i/t)");
+            reply = Console.ReadLine();
+            if (reply.ToLower() == "i")
+                MAKE_IMAGE = true;
+            if (MAKE_IMAGE)
+            {
+                Form screen = new Form();
+                screen.Text = "Pascal Triangle with " + ROWS + " rows, mod " + MOD;
+                Bitmap pic = tri.Image();
+                GDI32.SaveImage(pic);
+                screen.SetBounds(30, 30, pic.Width + 16, pic.Height  + 38);
+                screen.Show();
+                screen.SetDesktopLocation(0, 0);
+                Graphics g = Graphics.FromHwnd(screen.Handle);
+                g.DrawImage(pic, 0, 0);
+            }
+            else
+                tri.Print();
+            Console.ReadLine();
+        }
+    }
+
+    class GDI32
+    { //http://www.c-sharpcorner.com/UploadFile/perrylee/ScreenCapture11142005234547PM/ScreenCapture.aspx
+        [DllImport("GDI32.dll")]
+        public static extern bool BitBlt(int hdcDest, int nXDest, int nYDest, int nWidth, int nHeight, int hdcSrc, int nXSrc, int nYSrc, int dwRop);
+        [DllImport("GDI32.dll")]
+        public static extern int CreateCompatibleBitmap(int hdc, int nWidth, int nHeight);
+        [DllImport("GDI32.dll")]
+        public static extern int CreateCompatibleDC(int hdc);
+        [DllImport("GDI32.dll")]
+        public static extern bool DeleteDC(int hdc);
+        [DllImport("GDI32.dll")]
+        public static extern bool DeleteObject(int hObject);
+        [DllImport("GDI32.dll")]
+        public static extern int GetDeviceCaps(int hdc, int nIndex);
+        [DllImport("GDI32.dll")]
+        public static extern int SelectObject(int hdc, int hgdiobj);
+        [DllImport("User32.dll")]
+        public static extern int GetDesktopWindow();
+        [DllImport("User32.dll")]
+        public static extern int GetWindowDC(int hWnd);
+        [DllImport("User32.dll")]
+        public static extern int ReleaseDC(int hWnd, int hDC);
+
+        static int pictNumber = 0;
+
+        public static Bitmap CaptureScreen()
+        {
+            int hdcSrc = GetWindowDC(GetDesktopWindow()),
+            hdcDest = GDI32.CreateCompatibleDC(hdcSrc),
+            hBitmap = GDI32.CreateCompatibleBitmap(
+                            hdcSrc,
+                            GDI32.GetDeviceCaps(hdcSrc, 8),
+                            GDI32.GetDeviceCaps(hdcSrc, 10));
+            GDI32.SelectObject(hdcDest, hBitmap);
+            GDI32.BitBlt(hdcDest, 0, 0, GDI32.GetDeviceCaps(hdcSrc, 8),
+            GDI32.GetDeviceCaps(hdcSrc, 10), hdcSrc, 0, 0, 0x00CC0020);
+            Bitmap screen = Image.FromHbitmap(new IntPtr(hBitmap));
+            Cleanup(hBitmap, hdcSrc, hdcDest);
+            return screen;
+        }
+        private static void Cleanup(int hBitmap, int hdcSrc, int hdcDest)
+        {
+            ReleaseDC(GetDesktopWindow(), hdcSrc);
+            GDI32.DeleteDC(hdcDest);
+            GDI32.DeleteObject(hBitmap);
+        }
+        public static void SaveImage(Bitmap image)
+        {
+            // Puts the file into "debugging output"
+            pictNumber++;
+            image.Save("todelete" + pictNumber + ".bmp", ImageFormat.Bmp);
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,104 @@
+﻿// PascalsTriangle, by Carl Walsh Sept 2010
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Pascals
+{
+    class Triangle
+    {
+        private List<int[]> rows;
+        private int MOD;
+        private int ROWS;
+        public Triangle(int numRows, int mod)
+        {
+            ROWS = numRows;
+            MOD = mod;
+            rows = new List<int[]>();
+            for(int i = 0; i < numRows; ++i)
+            {
+                rows.Add(new int[i+1]);
+                if(i == 0)
+                    rows[i][0] = 1;
+                else
+                    for(int n = 0; n <= i; n++)
+                        rows[i][n] = getSum(i,n, mod);
+            }
+        }
+
+        private int getSum(int i, int n, int mod)
+        {
+            int[] row = rows[i-1];
+            int left = 0;
+            if(n != 0)
+                left = row[n-1];
+            int right = 0;
+            if(n < row.Count())
+                right = row[n];
+            return (right + left) % mod;
+        }
+
+        public Bitmap Image()
+        {
+            //There will be a square dot for each number, of dynamically chosen size
+            int DOT_SIZE = 625 / ROWS; //the pixel dimension of a dot
+            if (DOT_SIZE > 50)
+                DOT_SIZE = 50;
+            if (DOT_SIZE < 1)
+                DOT_SIZE = 1;
+
+            Bitmap ans = new Bitmap(ROWS * DOT_SIZE, ROWS * DOT_SIZE);
+            for (int y = 0; y < ans.Height; ++y)
+                for (int x = 0; x < ans.Width; ++x)
+                    ans.SetPixel(x, y, Color.White);
+            //0 is black, and the other colors are rainbow
+            Color[] color = new Color[MOD];
+            color[0] = Color.White;
+            for (int i = 1; i < MOD; ++i)
+                color[i] = ColorHandler.HSVtoColor(255 * (i - 1) / MOD, 255, 255);
+            //TODO get rid of
+            for (int r = 0; r < ROWS; ++r)
+                for (int c = 0; c <= r; ++c)
+                    changeDot(c, r, ROWS, color[rows[r][c]], DOT_SIZE, ans);
+            return ans;
+        }
+
+        private void changeDot(int column, int row, int ROWS,
+                               Color c, int dotSize, Bitmap img)
+        {
+            int offset = dotSize * (ROWS - row - 1) / 2;
+            Point p = new Point(offset + column * dotSize, row * dotSize);
+            int x = 0;
+            while (x < dotSize)
+            {
+                p.Y = row * dotSize;
+                int y = 0;
+                while (y < dotSize)
+                {
+                    img.SetPixel(p.X, p.Y, c);
+                    ++y;
+                    ++p.Y;
+                }
+                ++x;
+                ++p.X;
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < rows.Count; ++i)
+            {
+                for (int j = 1; j < rows.Count - i; ++j)
+                    Console.Write(' ');
+                for (int n = 0; n < rows[i].Count(); ++n)
+                {
+                    Console.Write(rows[i][n]);
+                    Console.Write(' ');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
